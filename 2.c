47e1c8c7835c89dc557de1b096d7156c54237c7c
kgwalker <kgwalker@163.com>
2020-10-17 18:54:42 +0800

    Job02

---
+++ 2.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: 2.c
+    > Author: kgwalker
+    > Mail: kgwalker@163.com
+    > Created Time: 2020年10月17日 星期六 18时53分39秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	printf("hello...\n");
+	return 0;
+}
